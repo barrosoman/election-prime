76a63dbac0a238e96f9b99a8fbf80ee00339ef9f
rricardof <ricardoxdd28@outlook.com>
2021-12-02 14:41:28 -0300

    Added the support for Vue-Router

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,33 @@
-import { createApp } from "vue";
-import App from "./App.vue";
+import { createApp } from 'vue'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
-createApp(App).mount("#app");
+import App from './App.vue'
+import Dashboard from './pages/Dashboard.vue'
+import Data from './pages/Data.vue'
+
+const routes = [
+  {
+    path: '/',
+    component: Dashboard
+  },
+  {
+    path: '/dashboard',
+    component: Dashboard
+  },
+  {
+    path: '/data',
+    component: Data
+  }
+]
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes
+})
+
+const app = createApp(App)
+
+app.use(router)
+app.mount('#app')
+
+// createApp(App).mount('#app')
