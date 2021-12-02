55690218de07e9ccc51165052ddd71d201f4313b
rricardof <ricardoxdd28@outlook.com>
2021-12-02 13:15:28 -0300

    Changes the Vertical Sidebar to Horizontal Sidebar closes #5

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
-  <Sidebar />
+  <Header />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 
-import Sidebar from './components/Sidebar.vue'
+import Header from './components/Header.vue'
 
 export default defineComponent({
   name: 'App',
   components: {
-    Sidebar
+    Header
   }
 })
 </script>
