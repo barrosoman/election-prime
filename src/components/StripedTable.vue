77560c9a0e9aa3355a6819e60665ef96227aeda0
rricardof <ricardoxdd28@outlook.com>
2021-12-04 16:27:13 -0300

    Striped table componentized

---
+++ src/components/StripedTable.vue
@@ -0,0 +1,42 @@
+<template>
+  <table class="striped-table">
+    <thead>
+      <slot name="head" />
+    </thead>
+    <tbody>
+      <slot name="body" />
+    </tbody>
+  </table>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({})
+</script>
+
+<style>
+.striped-table {
+  border-collapse: collapse;
+  border-spacing: 0;
+  width: 100%;
+  color: #495057;
+  font-family: 'IBM Plex Sans', sans-serif;
+  font-size: 14px;
+  font-weight: 400;
+  text-size-adjust: 100%;
+  text-align: center;
+  line-height: 21px;
+}
+
+.striped-table th,
+.striped-table td {
+  text-align: left;
+  padding: 16px;
+  border-bottom: 1px solid #e9e9ef;
+}
+
+.striped-table tr:nth-child(even) {
+  background-color: #f8f9fa;
+}
+</style>
