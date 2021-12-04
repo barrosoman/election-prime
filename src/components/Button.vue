884698989b30f9e80e0270ed84434563af2b68e3
rricardof <ricardoxdd28@outlook.com>
2021-12-04 16:13:54 -0300

    Removed duplicated styles

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -56,27 +56,4 @@ export default defineComponent({
   border-color: #7a7fdc;
   text-decoration: none;
 }
-
-.button {
-  padding-top: 1.75rem;
-  display: flex;
-  justify-content: center;
-}
-
-.button-link {
-  display: inline-block;
-  font-weight: 400;
-  line-height: 1.5;
-  text-align: center;
-  vertical-align: middle;
-  cursor: pointer;
-  padding: 0.47rem 0.75rem;
-  font-size: 0.875rem;
-  border-radius: 0.25rem;
-  box-shadow: 0 2px 6px 0 rgba(127, 127, 220, 1);
-  color: #fff;
-  background-color: #7a7fdc;
-  border-color: #7a7fdc;
-  text-decoration: none;
-}
 </style>
