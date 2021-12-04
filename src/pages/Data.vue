a04203aac4338d70f0c37b94507ed0926333ba0f
rricardof <ricardoxdd28@outlook.com>
2021-12-04 16:52:26 -0300

    Removed some unused information

--- src/pages/Data.vue
+++ src/pages/Data.vue
@@ -9,7 +9,6 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import { formatSimpleDate } from '@/util/dateFormatter'
 
 import Header from '@/components/Header.vue'
 import FileUploaderAndHistory from '@/components/data/FileUploaderAndHistory.vue'
@@ -22,29 +21,6 @@ export default defineComponent({
     FileUploaderAndHistory,
     TableInputData,
     TableValue
-  },
-  data() {
-    return {
-      fileSentHistory: null as string | null
-    }
-  },
-  mounted() {
-    this.updateFileSentHistory()
-  },
-  methods: {
-    formatSimpleDate,
-    updateFileSentHistory() {
-      const key = 'prob:file_history'
-      const fileSentHistoryString = localStorage.getItem(key)
-
-      if (fileSentHistoryString === null) this.fileSentHistory = null
-      else this.fileSentHistory = JSON.parse(fileSentHistoryString)
-    },
-    cleanFileSentHistory() {
-      const key = 'prob:file_history'
-      localStorage.removeItem(key)
-      this.fileSentHistory = null
-    }
   }
 })
 </script>
@@ -63,39 +39,5 @@ export default defineComponent({
 .mt-3 {
   margin-top: 1.5rem !important;
   margin-bottom: 1.5rem !important;
-}
-
-.no-data-sent {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  opacity: 0.75;
-}
-
-.no-data-sent svg {
-  width: 36px;
-  height: 36px;
-  margin-top: 1.5rem;
-  margin-bottom: 0.75rem;
-}
-
-.no-data-sent h3 {
-  color: #495057;
-  font-size: 1.3125rem;
-  font-weight: 400;
-  margin-bottom: 1.5rem;
-}
-
-.clean-file-history-container {
-  display: flex;
-  justify-content: flex-end;
-}
-
-.clean-file-history-icon {
-  background: #e9e9ef;
-  border-radius: 6px;
-  padding: 0.5rem;
-  box-sizing: content-box;
-  cursor: pointer;
 }
 </style>
