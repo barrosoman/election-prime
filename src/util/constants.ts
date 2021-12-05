7a9d277b1c08619ae2351aafa6c32858028611a1
rricardof <ricardoxdd28@outlook.com>
2021-12-05 16:10:08 -0300

    Added the DATA_KEY

--- src/util/constants.ts
+++ src/util/constants.ts
@@ -2,3 +2,4 @@ const registerKey = (namespace: string, key: string): string =>
   `${namespace}:${key}`
 
 export const FILE_HISTORY_KEY = registerKey('prob', 'file_history')
+export const DATA_KEY = registerKey('prob', 'data')
