10ea05d77bd7df74749841ef779a7388d524622f
rricardof <ricardoxdd28@outlook.com>
2021-12-09 14:21:23 -0300

    Insights with relation by total votes and age group has been added

--- src/pages/Insights.vue
+++ src/pages/Insights.vue
@@ -38,10 +38,24 @@
       />
     </Card>
     <Card
+      v-if="showInsights"
       class="mt-3"
       title="Insights com relação à intenção total de votos"
       subtitle="Consulte seus resultados com relação à intenção total de votos"
     >
+      <span class="votes-insight">{{ calculateTotalVotesInsight() }}</span>
+    </Card>
+    <Card
+      v-if="showInsights"
+      class="mt-3"
+      title="Insights com relação à intenção de votos por Faixa Etária"
+      subtitle="Consulte seus resultados com relação à intenção de votos por faixa etária"
+    >
+      {{ calculateAgeVotesInsight() }}
+      <div class="votes-container">
+        <span class="votes-insight">{{ mostGroupAgeMessage }}</span>
+        <span class="votes-insight">{{ leastGroupAgeMessage }}</span>
+      </div>
     </Card>
   </Page>
 </template>
@@ -68,7 +82,10 @@ export default defineComponent({
       dataInsighter: undefined as DataInsighter | undefined,
       presidentNames: [] as string[],
       governorsNames: [] as string[],
-      unrecgonizedCandidate: false
+      unrecgonizedCandidate: false,
+      showInsights: false,
+      mostGroupAgeMessage: '',
+      leastGroupAgeMessage: ''
     }
   },
   mounted() {
@@ -95,6 +112,91 @@ export default defineComponent({
         this.candidate,
         this.dataVisualizer as DataVisualizer
       )
+      this.showInsights = true
+    },
+    /**
+     * This method returns a message that informs the user
+     * the classification in that poll.
+     */
+    calculateTotalVotesInsight(): string {
+      if (this.dataInsighter === undefined) throw 'data insighter undefined'
+
+      const position = this.dataInsighter.getTotalVotesPosition()
+      let message = ''
+
+      if (position === 1) message = 'Excelente!'
+      else if (position === 2) message = 'Opa, estamos quase lá!'
+      else if (position === 3) message = 'Vamos lá, iremos melhorar!'
+      else if (position === 4) message = 'Não desamine, estamos muito perto!'
+      else if (position >= 5 && position <= 10)
+        message = 'Poxa, não tá bom, mas dá para melhorar!'
+      else if (position > 10 && position <= 20)
+        message = 'Pera, pera, pera, ajudaremos a chegar no TOP 10.'
+      else if (position > 20) message = 'É, tá duro!'
+
+      return `Você ficou em #${position} nessa pesquisa. ${message}`
+    },
+    /**
+     * This method returns a message that informs the age group
+     * that most voted in the user
+     */
+    calculateAgeVotesInsight(): void {
+      if (this.dataInsighter === undefined) throw 'data insighter undefined'
+
+      let mostGroupAgeMessage = ''
+      let leastGroupAgeMessage = ''
+
+      const [mostGroupAge, leastGroupAge] =
+        this.dataInsighter.getAgeVotesMostAndLeast()
+
+      if (mostGroupAge === '16')
+        mostGroupAgeMessage = 'Os jovens estão loucos por você!'
+      else if (mostGroupAge === '17')
+        mostGroupAgeMessage = 'Os pré-adultos estão te adorando!'
+      else if (mostGroupAge === '18-20')
+        mostGroupAgeMessage = 'Os jovens adultos te apoaim!'
+      else if (mostGroupAge === '21-24')
+        mostGroupAgeMessage = 'Você está com o pessoal dos 21 aos 24 anos!'
+      else if (mostGroupAge === '25-34')
+        mostGroupAgeMessage =
+          'Com certeza, a população dos 25 aos 34 anos o apoia!'
+      else if (mostGroupAge === '35-44')
+        mostGroupAgeMessage = 'Os adultos estão te apoiando!'
+      else if (mostGroupAge === '45-59')
+        mostGroupAgeMessage = 'A meia-idade está com você!'
+      else if (mostGroupAge === '60-69')
+        mostGroupAgeMessage = 'A população dos 60 ao 69 anos está com você!'
+      else if (mostGroupAge === '70-79')
+        mostGroupAgeMessage = 'Os idosos estão te adorando!'
+      else if (mostGroupAge === '>79')
+        mostGroupAgeMessage = 'O apoio dos com mais de 79 anos é seu!'
+
+      if (leastGroupAge === '16')
+        leastGroupAgeMessage = 'Necessitamos focar mais na comunidade jovem!'
+      else if (leastGroupAge === '17')
+        leastGroupAgeMessage = 'É preciso focar nos pré-adultos!'
+      else if (leastGroupAge === '18-20')
+        leastGroupAgeMessage = 'É vital focar nos jovens adultos!'
+      else if (leastGroupAge === '21-24')
+        leastGroupAgeMessage = 'Primordial focar no pessoal dos 21 aos 24 anos!'
+      else if (leastGroupAge === '25-34')
+        leastGroupAgeMessage =
+          'Devemos destacar a população dos 25 aos 34 anos!'
+      else if (leastGroupAge === '35-44')
+        leastGroupAgeMessage =
+          'Algo deve ser feito para que os adultos o apoie!'
+      else if (leastGroupAge === '45-59')
+        leastGroupAgeMessage = 'É fato, a meia-idade não está te apoiando!'
+      else if (leastGroupAge === '60-69')
+        leastGroupAgeMessage =
+          'A faixa etária dos 60 aos 69 anos está difícil para você!'
+      else if (leastGroupAge === '70-79')
+        leastGroupAgeMessage = 'Devemos focar nas questões dos idosos!'
+      else if (leastGroupAge === '>79')
+        leastGroupAgeMessage = 'Os com mais de 79 anos não o apoiam!'
+
+      this.mostGroupAgeMessage = mostGroupAgeMessage
+      this.leastGroupAgeMessage = leastGroupAgeMessage
     }
   }
 })
@@ -155,5 +257,15 @@ export default defineComponent({
 .mt-3 {
   margin-top: 1.5rem !important;
   margin-bottom: 1.5rem !important;
+}
+
+.votes-container {
+  display: flex;
+  flex-direction: column;
+}
+
+.votes-insight {
+  color: #030303;
+  margin-bottom: 0.1725rem;
 }
 </style>
--- src/util/dataInsighter.ts
+++ src/util/dataInsighter.ts
@@ -1,5 +1,29 @@
-import { DataInfo } from '@/models/DataInfo'
+import { DataInfo, SeriesData } from '@/models/DataInfo'
 import { DataVisualizer } from '@/util/dataVisualizer'
+
+interface TotalVotes {
+  /**
+   * The candidate that has this votes.
+   */
+  candidate: string
+
+  /**
+   * The amount of votes that the candidate received.
+   */
+  votes: number
+}
+
+interface AgeVotes {
+  /**
+   * The age group that has this votes.
+   */
+  age: string
+
+  /**
+   * The amount of votes that the age group received.
+   */
+  votes: number
+}
 
 export class DataInsighter {
   /**
@@ -18,10 +42,118 @@ export class DataInsighter {
    */
   readonly dataVisualizer: DataVisualizer
 
+  /**
+   * This variable indicates if the {@code candidate} represents a president or not.
+   * Then, if this is {@code true} then the {@code candidate} is a president. Otherwise,
+   * it is a governor.
+   */
+  readonly candidatePresident: boolean
+
   constructor(candidate: string, dataVisualizer: DataVisualizer) {
     this.candidate = candidate
     this.dataInfoArray = dataVisualizer.getVisualizedData()
     this.dataVisualizer = dataVisualizer
+    this.candidatePresident = this.dataVisualizer
+      .toPresidentsName()
+      .some((name) => name === candidate)
+  }
+
+  getTotalVotesPosition(): number {
+    const totalVotes: TotalVotes[] = this.getTotalVotes()
+    totalVotes.sort((a, b) => b.votes - a.votes)
+
+    return (
+      totalVotes.findIndex(
+        (totalVote: TotalVotes) => totalVote.candidate === this.candidate
+      ) + 1
+    )
+  }
+
+  getAgeVotesMostAndLeast(): [string, string] {
+    const ageVotes: AgeVotes[] = this.getAgeVotes()
+    ageVotes.sort((a, b) => b.votes - a.votes)
+
+    return [ageVotes[0].age, ageVotes[ageVotes.length - 1].age]
+  }
+
+  private getTotalVotes(): TotalVotes[] {
+    const totalVotes: TotalVotes[] = []
+    let candidateNames: string[]
+    let candidateVotes: number[]
+
+    if (this.candidatePresident) {
+      candidateNames = this.dataVisualizer.toPresidentsName()
+      candidateVotes = this.dataVisualizer.toPresidentsVote()
+    } else {
+      candidateNames = this.dataVisualizer.toGovernorsName()
+      candidateVotes = this.dataVisualizer.toGovernorsVote()
+    }
+
+    if (candidateNames.length !== candidateVotes.length)
+      throw `Candidate names length ${candidateNames.length} cannot differ from candidate votes length ${candidateVotes.length}`
+
+    for (let index = 0; index < candidateNames.length; index++)
+      totalVotes.push({
+        candidate: candidateNames[index],
+        votes: candidateVotes[index]
+      })
+
+    return totalVotes
+  }
+
+  private getAgeVotes(): AgeVotes[] {
+    let candidatesAgeVotes: SeriesData[]
+
+    if (this.candidatePresident)
+      candidatesAgeVotes = this.dataVisualizer.toPresidentsAgeGroupVote()
+    else candidatesAgeVotes = this.dataVisualizer.toGovernorsAgeGroupVote()
+
+    const candidateAgeVotes: SeriesData = candidatesAgeVotes.filter(
+      (seriesData) => seriesData.name === this.candidate
+    )[0]
+
+    return [
+      {
+        age: '16',
+        votes: candidateAgeVotes.data[0]
+      },
+      {
+        age: '17',
+        votes: candidateAgeVotes.data[1]
+      },
+      {
+        age: '18-20',
+        votes: candidateAgeVotes.data[2]
+      },
+      {
+        age: '21-24',
+        votes: candidateAgeVotes.data[3]
+      },
+      {
+        age: '25-34',
+        votes: candidateAgeVotes.data[4]
+      },
+      {
+        age: '35-44',
+        votes: candidateAgeVotes.data[5]
+      },
+      {
+        age: '45-59',
+        votes: candidateAgeVotes.data[6]
+      },
+      {
+        age: '60-69',
+        votes: candidateAgeVotes.data[7]
+      },
+      {
+        age: '70-79',
+        votes: candidateAgeVotes.data[8]
+      },
+      {
+        age: '>79',
+        votes: candidateAgeVotes.data[9]
+      }
+    ]
   }
 
   /**
