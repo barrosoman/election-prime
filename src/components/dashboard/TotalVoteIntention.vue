b7d611ba0f148a820c693a3f18a365502c32b5c5
rricardof <ricardoxdd28@outlook.com>
2022-01-12 12:46:46 -0300

    Added some statistics measures for the total vote intention graph.
    * The measures calculated for the total vote intention are: mean, median, mode, standard deviation, lower quartile, upper quartile, interquartile range, asymmetry, kurtosis, range and coefficient of variation.

--- src/components/dashboard/TotalVoteIntention.vue
+++ src/components/dashboard/TotalVoteIntention.vue
@@ -5,12 +5,66 @@
       subtitle: `Consulte as intenções de voto à ${this.target.toLowerCase()}`
     }"
   >
-    <apexchart
-      width="80%"
-      type="bar"
-      :options="chartOptions"
-      :series="series"
-    ></apexchart>
+    <template v-slot:default>
+      <apexchart
+        width="80%"
+        type="bar"
+        :options="chartOptions"
+        :series="series"
+      ></apexchart>
+    </template>
+    <template v-slot:footer>
+      <StripedTable>
+        <template v-slot:body>
+          <tr>
+            <th>Média</th>
+            <td>{{ mean }}</td>
+          </tr>
+          <tr>
+            <th>Mediana</th>
+            <td>{{ median }}</td>
+          </tr>
+          <tr>
+            <th>Moda</th>
+            <td>{{ Number.isNaN(mode) ? '*' : mode }}</td>
+          </tr>
+          <tr>
+            <th>Desvio Padrão</th>
+            <td>{{ standardDeviation.toFixed(2) }}</td>
+          </tr>
+          <tr>
+            <th>Q1</th>
+            <td>{{ quartile[0] }}</td>
+          </tr>
+          <tr>
+            <th>Q3</th>
+            <td>{{ quartile[1] }}</td>
+          </tr>
+          <tr>
+            <th>IIQ</th>
+            <td>{{ quartile[1] - quartile[0] }}</td>
+          </tr>
+          <tr>
+            <th>Assimetria</th>
+            <td>
+              {{ ((mean - median) / standardDeviation).toFixed(2) }}
+            </td>
+          </tr>
+          <tr>
+            <th>Curtose</th>
+            <td>{{ kurtosis.toFixed(2) }}</td>
+          </tr>
+          <tr>
+            <th>Amplitude</th>
+            <td>{{ range }}</td>
+          </tr>
+          <tr>
+            <th>Coeficiente de Variação</th>
+            <td>{{ ((standardDeviation / mean) * 100.0).toFixed(2) }}%</td>
+          </tr>
+        </template>
+      </StripedTable>
+    </template>
   </Card>
 </template>
 
@@ -18,10 +72,14 @@
 import { defineComponent } from 'vue'
 
 import Card from '@/components/Card.vue'
+import StripedTable from '@/components/StripedTable.vue'
+
+import { DataStatistics } from '@/util/dataStatistics'
 
 export default defineComponent({
   components: {
-    Card
+    Card,
+    StripedTable
   },
   props: {
     target: {
@@ -58,6 +116,41 @@ export default defineComponent({
           data: this.votes
         }
       ]
+    }
+  },
+  computed: {
+    /* [!] Make a copy of the votes but sorted. */
+    /* Now use that copy and not the votes directly */
+    /* to not affect the graph distrtibution. [!] */
+    sortedVotes(): number[] {
+      console.log('Votes Called!')
+      return ([...this.votes] as number[]).sort(
+        DataStatistics.sortingComparator
+      )
+    },
+    mean() {
+      return DataStatistics.mean(this.sortedVotes as number[], true)
+    },
+    median() {
+      return DataStatistics.median(this.sortedVotes as number[], true)
+    },
+    mode() {
+      return DataStatistics.mode(this.sortedVotes as number[])
+    },
+    standardDeviation(): number {
+      return DataStatistics.standardDeviation(
+        this.sortedVotes as number[],
+        true
+      )
+    },
+    quartile() {
+      return DataStatistics.quartile(this.sortedVotes as number[], true)
+    },
+    range() {
+      return DataStatistics.range(this.sortedVotes as number[])
+    },
+    kurtosis() {
+      return DataStatistics.kurtosis(this.sortedVotes as number[])
     }
   }
 })
