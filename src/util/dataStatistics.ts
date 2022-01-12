00dbf991c1a1750c1a44d36d0dbdd43c1f49c513
rricardof <ricardoxdd28@outlook.com>
2022-01-12 09:25:48 -0300

    Added the `DataStatistics` class.
    * This class will calculate sample measures such that can be done a sample analysis from that information.
    
    * The methods added are `mean`, `median`, `mode`, `quartile`, `standardDeviation`, `asymmetry` and `range`.

---
+++ src/util/dataStatistics.ts
@@ -0,0 +1,181 @@
+export default class DataStatistics {
+  /**
+   * Returns the mean of a sample.
+   *
+   * Given a sample populated with n numbers. It is returned the
+   * mean of that sample, calculated by summing up these numbers
+   * and dividing that result by n (the amount of numbers in the sample).
+   *
+   * @param {Number[]} sample the sample
+   *
+   * @returns {Number} the mean of a sample
+   */
+  static mean(sample: number[]): number {
+    return sample.sort().reduce((prev, curr) => prev + curr) / sample.length
+  }
+
+  /**
+   * Returns the median of a sample.
+   *
+   * Given a sample populated with n numbers. It is returned the
+   * median of that sample, that represents the central number of
+   * that sample but sorted.
+   *
+   * If the amount of numbers in the sample (n) is even, the median
+   * is the mean of the two central numbers. Otherwise, it is returned
+   * the central number.
+   *
+   * @param {Number[]} sample the sample
+   * @param {Boolean} sorted if it is true represents that the sample is
+   *                         already sorted. Otherwise, the method will
+   *                         sort the sample.
+   *
+   * @returns {Number} the median of a sample
+   */
+  static median(sample: number[], sorted?: boolean): number {
+    /* Sort the sample if it is not sorted */
+    if (!sorted) sample.sort()
+
+    const n = sample.length
+
+    /* Check if the amount of numbers in the sample */
+    /* is a even number. Then the median is the mean */
+    /* of the two central numbers in the sample. */
+    if (n % 2 === 0) return 0.5 * (sample[n / 2 - 1] + sample[n / 2])
+
+    return sample[(n - 1) / 2]
+  }
+
+  /**
+   * Returns the mode of a sample.
+   *
+   * Given a sample populated with n numbers. The mode of a sample is the
+   * most occurring number in that sample.
+   *
+   * @param {Number} sample the sample
+   *
+   * @returns the mode of the sample
+   */
+  static mode(sample: number[]): number {
+    const countingMap: any = {}
+
+    for (const sampleValue of sample)
+      countingMap[sampleValue] = countingMap[sampleValue] + 1 || 1
+
+    let mode = sample[0]
+
+    for (const sampleValue of sample)
+      if (countingMap[mode] < countingMap[sampleValue]) mode = sampleValue
+
+    return mode
+  }
+
+  /**
+   * Returns the lower and upper quartile of a sample.
+   *
+   * Given a sample populated with n numbers. The lower quartile of the sample
+   * is the median of the lower half the sample. Likewise, the upper quartile of
+   * the sample is the median of the upper half of the sample.
+   *
+   * @param {Number} sample the sample
+   *
+   * @returns the lower and upper quartile of the sample.
+   */
+  static quartile(sample: number[]): [number, number] {
+    /* Sort the sample */
+    sample.sort()
+
+    const n = sample.length
+
+    /* Check if the amount of numbers in the sample */
+    /* is a even number. */
+    if (n % 2 === 0) {
+      const halfN = n / 2
+      return [
+        this.median(sample.slice(0, halfN), true),
+        this.median(sample.slice(halfN, n), true)
+      ]
+    }
+
+    const medianIndex = (n - 1) / 2
+    return [
+      this.median(sample.slice(0, medianIndex + 1), true),
+      this.median(sample.slice(medianIndex, n), true)
+    ]
+  }
+
+  /**
+   * Returns the standard deviation of a sample.
+   *
+   * Given a sample populated with n numbers. It is returned the
+   * standard deviation of that sample, that represents an estimative
+   * value of the deviation of the sample values from the sample mean.
+   *
+   * @param {Number[]} sample the sample
+   *
+   * @returns {Number} the standard deviation of a sample
+   */
+  static standardDeviation(sample: number[]): number {
+    const n = sample.length
+    const sxx =
+      sample.sort().reduce((prev, curr) => prev + Math.pow(curr, 2)) -
+      n * Math.pow(this.mean(sample), 2)
+    return Math.sqrt(sxx / (n - 1))
+  }
+
+  /**
+   * Returns the asymmetry of a sample.
+   *
+   * If one of the values mean, median or standard deviation has not been specified,
+   * the method will calculate all these values before calculate the asymmetry.
+   *
+   * @param {Number[]} sample the sample
+   * @param {Number} [mean] the mean of that sample
+   * @param {Number} [median] the median of that sample
+   * @param {Number} [standardDeviation] the standard deviation of that sample
+   *
+   * @returns the asymmetry of the sample
+   */
+  static asymmetry(
+    sample: number[],
+    mean?: number,
+    median?: number,
+    standardDeviation?: number
+  ): number {
+    if (!mean || !median || !standardDeviation) {
+      mean = this.mean(sample)
+      median = this.median(sample)
+      standardDeviation = this.standardDeviation(sample)
+    }
+
+    return (3.0 * (mean - median)) / standardDeviation
+  }
+
+  /**
+   * Returns the range of a sample.
+   *
+   * Given a sample populated with n numbers. It is returned the range
+   * of that sample, calculated by the difference between the greatest
+   * and lowest value in that sample.
+   *
+   * If the minimum value or maximum valuue has not been specified. The
+   * method will calculate those values from the sample.
+   *
+   * @param {Number[]} sample the sample
+   * @param {Number} min the minimum value of that sample
+   * @param {Number} max the maximum value of that sample
+   *
+   * @returns the range of that sample
+   */
+  static range(sample: number[], min?: number, max?: number): number {
+    if (!min || !max) {
+      /* Sort the sample */
+      sample.sort()
+
+      min = sample[0]
+      max = sample[sample.length - 1]
+    }
+
+    return max - min
+  }
+}
