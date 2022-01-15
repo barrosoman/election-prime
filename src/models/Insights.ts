b2f9d4bf3efa74f2ef1a614616934e609853b82d
rricardof <ricardoxdd28@outlook.com>
2022-01-15 11:28:27 -0300

    Crate the `InsightsMessage` interface
    * That interface will store all messages that must be shown in the Insights page.

---
+++ src/models/Insights.ts
@@ -0,0 +1,16 @@
+export interface InsightsMessage {
+  mostGroupAgeMessage: string
+  leastGroupAgeMessage: string
+  mostReligionVotesMessage: string
+  leastReligionVotesMessage: string
+  mostEthnicityVotesMessage: string
+  leastEthnicityVotesMessage: string
+  mostRegionVotesMessage: string
+  leastRegionVotesMessage: string
+  mostScholarityVotesMessage: string
+  leastScholarityVotesMessage: string
+  mostSexVotesMessage: string
+  leastSexVotesMessage: string
+  mostIncomeVotesMessage: string
+  leastIncomeVotesMessage: string
+}
